8699fd89f131d733ae00fd5f753c97a8d8eaf6c4
Suprio-Das <priyamd507@gmail.com>
2025-01-31 01:06:52 +0600

    Three more Components and Routes setup

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../Home/Home";
 import Root from "../Root/Root";
+import Login from "../Login/Login";
+import SignUp from "../SignUp/SignUp";
 
 
 export const router = createBrowserRouter([
@@ -11,6 +13,14 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
             }
         ]
     }
