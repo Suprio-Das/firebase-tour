dcd211daf1c5619e08cfddb1db14c206ce0c3727
Suprio-Das <priyamd507@gmail.com>
2025-01-31 03:10:13 +0600

    Email Verification added in about

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,8 @@ import firebaseImage from "../images/firebase.png";
 import firebaseLogo from "../images/firebaseLogo.png";
 import firebaseEmail from "../images/firebaseEmail.png";
 import firebaseGoogle from "../images/firebaseGoogle.png";
+import firebaseGit from "../images/firebaseGit.png";
+import firebaseEmailVerification from "../images/firebaseEmailVerification.png";
 const Home = () => {
     return (
         <div className="w-[80%] mx-auto lg:my-11">
@@ -53,14 +55,23 @@ const Home = () => {
                     {/* GitHub Authentication */}
                     <div className="bg-neutral-100 hover:shadow-md transition ease-in-out h-40 flex flex-col">
                         <div className="flex-1 flex items-center justify-center p-2">
-                            <img src={firebaseGoogle} alt="firebase_email" className="w-20" />
+                            <img src={firebaseGit} alt="firebase_email" className="w-20" />
                         </div>
                         <div className="bg-black mt-auto p-2">
-                            <h1 className="text-center text-md font-semibold text-white">Google Authentication</h1>
+                            <h1 className="text-center text-md font-semibold text-white">GitHub Authentication</h1>
+                        </div>
+                    </div>
+
+                    {/* Email Verification */}
+                    <div className="bg-neutral-100 hover:shadow-md transition ease-in-out h-40 flex flex-col">
+                        <div className="flex-1 flex items-center justify-center p-2">
+                            <img src={firebaseEmailVerification} alt="firebase_email" className="w-20" />
+                        </div>
+                        <div className="bg-black mt-auto p-2">
+                            <h1 className="text-center text-md font-semibold text-white">Email Verification</h1>
                         </div>
                     </div>
                 </div>
-
             </section>
         </div>
     );
