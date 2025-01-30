77f32c917b5bcb896bdc7708fe7c5b6af902ce33
Suprio-Das <priyamd507@gmail.com>
2025-01-31 01:02:37 +0600

    React Router Installed and Basic Route Setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../Home/Home";
+import Root from "../Root/Root";
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
