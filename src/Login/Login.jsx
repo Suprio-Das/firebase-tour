c4314e2fd003fcc56b57b61edc8b1274cd483ff3
Suprio-Das <priyamd507@gmail.com>
2025-01-31 16:00:11 +0600

    Login Functionality added with auto Navigation

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,11 +1,35 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { IoPersonCircleSharp } from "react-icons/io5";
+import { auth } from "../Firebase/Firebase.config";
+import { useState } from "react";
+import { redirect, useNavigate } from "react-router";
 
 const Login = () => {
+    const [loginError, setLoginError] = useState(false);
+    const navigate = useNavigate();
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                console.log(result)
+                if (!result.user.emailVerified) {
+                    setLoginError('Please Verify Your Email First.')
+                    return;
+                }
+                else {
+                    navigate("/")
+                }
+            })
+
+    }
     return (
         <div className="flex flex-col items-center min-h-screen lg:mt-[-30px] justify-center mx-auto">
             <div className="max-w-xl border-2 border-dark p-5 rounded-lg">
                 <h1 className="flex items-center justify-center text-xl font-semibold"><IoPersonCircleSharp className="text-3xl mr-2" /> Login</h1>
-                <form>
+                <form onSubmit={handleLogin}>
                     <div className="mt-5">
                         <label htmlFor="email">
                             Email
