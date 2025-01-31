4e39229e3802903a595dd3a808f5be04a04575db
Suprio-Das <priyamd507@gmail.com>
2025-01-31 13:48:46 +0600

    Password validation added using regex and error message handle using state

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { IoPersonCircleSharp } from "react-icons/io5";
 const SignUp = () => {
+    const [errorMessage, setErrorMessage] = useState(false);
     const handleRegister = (e) => {
         e.preventDefault();
         const name = e.target.name.value;
@@ -7,6 +9,16 @@ const SignUp = () => {
         const profile = e.target.profile.value;
         const password = e.target.password.value;
         console.log(name, email, profile, password)
+
+        // reseting state
+        setErrorMessage(false);
+
+        // Validating password
+        const passwordRegex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/";
+        if (!password.test(passwordRegex)) {
+            setErrorMessage(true);
+            return;
+        }
     }
     return (
         <div className="flex flex-col items-center min-h-screen lg:mt-[-30px] justify-center mx-auto">
@@ -17,25 +29,25 @@ const SignUp = () => {
                         <label htmlFor="name">
                             Name
                         </label>
-                        <input type="text" placeholder="E.g. Suprio Das" name="name" className="input w-full" />
+                        <input type="text" placeholder="E.g. Suprio Das" name="name" className="input w-full" required />
                     </div>
                     <div className="mt-5">
                         <label htmlFor="email">
                             Email
                         </label>
-                        <input type="email" placeholder="abc@gmail.com" name="email" className="input w-full" />
+                        <input type="email" placeholder="abc@gmail.com" name="email" className="input w-full" required />
                     </div>
                     <div className="mt-5">
                         <label htmlFor="profile">
                             Profile
                         </label>
-                        <input type="text" placeholder="Upload Your Image Link" name="profile" className="input w-full" />
+                        <input type="text" placeholder="Upload Your Image Link" name="profile" className="input w-full" required />
                     </div>
                     <div className="mt-5">
                         <label htmlFor="profile">
                             Password
                         </label>
-                        <input type="password" placeholder="Create a strong password" name="password" className="input w-full" />
+                        <input type="password" placeholder="Create a strong password" name="password" className="input w-full" required />
                     </div>
                     <button type="submit" className="btn btn-dark w-full mt-5 btn-neutral">
                         Sign-up
