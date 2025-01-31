59ea80d4e4bc1ac7a23ab4d95f411eb5214705c7
Suprio-Das <priyamd507@gmail.com>
2025-01-31 16:32:49 +0600

    User Logged-in test in Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import { NavLink } from "react-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../Firebase/Firebase.config"
 
 const Header = () => {
+    const [user, setUser] = useState(null);
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setUser(user);
+        }
+        else {
+            console.log("User is not logged in.")
+        }
+    })
     return (
         <div>
             <div className="navbar bg-base-100 shadow-sm">
@@ -27,26 +39,31 @@ const Header = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img
-                                    alt="Tailwind CSS Navbar component"
-                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    {
+                        user ?
+                            <div className="dropdown dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img
+                                            alt="Tailwind CSS Navbar component"
+                                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                    </div>
+                                </div>
+                                <ul
+                                    tabIndex={0}
+                                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                    <li>
+                                        <a className="justify-between">
+                                            Profile
+                                        </a>
+                                    </li>
+                                    <li><a>Settings</a></li>
+                                    <li><a>Logout</a></li>
+                                </ul>
                             </div>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                </a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
-                    </div>
+                            :
+                            <button>Login</button>
+                    }
                 </div>
             </div>
         </div>
