865cc46fea03ae212646ed8002c5c822a44b1f4c
Suprio-Das <priyamd507@gmail.com>
2025-01-31 12:10:31 +0600

    Sign-up page heading added

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,7 +1,8 @@
+import { IoPersonCircleSharp } from "react-icons/io5";
 const SignUp = () => {
     return (
-        <div>
-            <h1>This is signup page.</h1>
+        <div className="flex items-center min-h-screen justify-center">
+            <h1 className="flex items-center text-xl font-semibold"><IoPersonCircleSharp className="text-3xl mr-2" /> Register Your Firebase Account!</h1>
         </div>
     );
 };
