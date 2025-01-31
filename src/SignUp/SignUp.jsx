0503a4637006f6484998c0ae07c428c3c9762e7c
Suprio-Das <priyamd507@gmail.com>
2025-01-31 12:20:50 +0600

    Email field added

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -4,12 +4,18 @@ const SignUp = () => {
         <div className="flex flex-col items-center min-h-screen justify-center mx-auto">
             <div className="max-w-xl">
                 <h1 className="flex items-center text-xl font-semibold"><IoPersonCircleSharp className="text-3xl mr-2" /> Register Your Firebase Account!</h1>
-                <form className="mt-5">
-                    <div>
+                <form>
+                    <div className="mt-5">
                         <label htmlFor="name">
                             Name
                         </label>
-                        <input type="text" placeholder="Type here" name="name" className="input w-full" />
+                        <input type="text" placeholder="E.g. Suprio Das" name="name" className="input w-full" />
+                    </div>
+                    <div className="mt-5">
+                        <label htmlFor="email">
+                            Email
+                        </label>
+                        <input type="email" placeholder="abc@gmail.com" name="email" className="input w-full" />
                     </div>
                 </form>
             </div>
