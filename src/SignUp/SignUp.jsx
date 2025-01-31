98404c395bbcfaaf7f8319e71da00371861587ee
Suprio-Das <priyamd507@gmail.com>
2025-01-31 12:18:31 +0600

    Registration Form Created and Name Field Added

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,8 +1,18 @@
 import { IoPersonCircleSharp } from "react-icons/io5";
 const SignUp = () => {
     return (
-        <div className="flex items-center min-h-screen justify-center">
-            <h1 className="flex items-center text-xl font-semibold"><IoPersonCircleSharp className="text-3xl mr-2" /> Register Your Firebase Account!</h1>
+        <div className="flex flex-col items-center min-h-screen justify-center mx-auto">
+            <div className="max-w-xl">
+                <h1 className="flex items-center text-xl font-semibold"><IoPersonCircleSharp className="text-3xl mr-2" /> Register Your Firebase Account!</h1>
+                <form className="mt-5">
+                    <div>
+                        <label htmlFor="name">
+                            Name
+                        </label>
+                        <input type="text" placeholder="Type here" name="name" className="input w-full" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
