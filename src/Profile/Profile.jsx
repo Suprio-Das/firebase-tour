d6c4617ed5ea7ef1d02256412e03b3600221acb9
Suprio-Das <priyamd507@gmail.com>
2025-01-31 23:51:45 +0600

    Edit Profile Button Added

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -22,9 +22,9 @@ const Profile = () => {
         <div className="w-[80%] mx-auto my-20">
             {
                 loading ? <p className="max-w-xl loading loading-spin min-h-screen justify-center items-center text-center"></p> :
-                    <div className="w-1/3 mx-auto bg-gray-100 p-3 ">
+                    <div className="w-1/3 mx-auto bg-gray-100 p-5">
                         <div className="flex justify-center">
-                            <img src={user.photoURL} alt="" className="w-28 h-28 rounded-full border-4" />
+                            <img src={user.photoURL} alt="" className="w-30 h-30 rounded-full border-4" />
                         </div>
                         <div>
                             <h2 className="text-center text-xl font-semibold my-2">{user.displayName}</h2>
@@ -33,6 +33,9 @@ const Profile = () => {
                         </div>
                     </div>
             }
+            <button className="btn btn-neutral w-1/3 mx-auto block my-3">
+                Edit Profile
+            </button>
         </div>
     );
 };
